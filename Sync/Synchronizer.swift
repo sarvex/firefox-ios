2dcc50cace0bfefe4a1e1297e49ab50ff00e1f3e
Richard Newman <rnewman@twinql.com>
2015-03-17 17:57:56 -0700

    synchronizer

--- Sync/Synchronizer.swift
+++ Sync/Synchronizer.swift
@@ -14,11 +14,20 @@ public class ClientsSynchronizer: Synchronizer {
     private let info: InfoCollections
     private let prefs: ProfilePrefs
 
+    private let prefix = "clients"
+    private let collection = "clients"
+
     required public init(info: InfoCollections, prefs: ProfilePrefs) {
         self.info = info
         self.prefs = prefs
     }
 
     public func synchronize() {
+        if let last = prefs.longForKey(self.prefix + "last") {
+            if last == info.modified(self.collection) {
+                // Nothing to do.
+                return;
+            }
+        }
     }
 }
