4f6c75258898118ed7f746073c3943fd60254b40
Richard Newman <rnewman@twinql.com>
2015-03-18 19:14:18 -0700

    Uh oh, I broke it.

--- SyncTests/LiveStorageClientTests.swift
+++ SyncTests/LiveStorageClientTests.swift
@@ -30,34 +30,91 @@ class LiveStorageClientTests : LiveAccountTest {
         return deferred
     }
 
+    func getToken(state: FirefoxAccountState) -> Deferred<Result<TokenServerToken>> {
+        if let married = state as? FirefoxAccountState.Married {
+            let audience = TokenServerClient.getAudienceForURL(ProductionSync15Configuration().tokenServerEndpointURL)
+            let clientState = FxAClient10.computeClientState(married.kB)
+            let client = TokenServerClient()
+            return client.token(married.generateAssertionForAudience(audience), clientState: clientState)
+        } else {
+            return Deferred(value: Result(failure: NSError(domain: "foo", code: 0, userInfo: nil)))
+        }
+    }
+
+    func getKeys(married: FirefoxAccountState.Married, token: TokenServerToken) -> Deferred<Result<Record<KeysPayload>>> {
+        let endpoint = token.api_endpoint
+        XCTAssertTrue(endpoint.rangeOfString("services.mozilla.com") != nil, "We got a Sync server.")
+
+        let cryptoURI = NSURL(string: endpoint + "/storage/crypto/")
+        let authorizer: Authorizer = {
+            (r: NSMutableURLRequest) -> NSMutableURLRequest in
+            let helper = HawkHelper(id: token.id, key: token.key.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
+            r.addValue(helper.getAuthorizationValueFor(r), forHTTPHeaderField: "Authorization")
+            return r
+        }
+
+        let keyBundle: KeyBundle = KeyBundle.fromKB(married.kB)
+        let keysFactory: (String) -> KeysPayload? = Keys(defaultBundle: keyBundle).factory("keys")
+
+        let workQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        let resultQueue = dispatch_get_main_queue()
+        let keysFetch = Sync15StorageClient(serverURI: cryptoURI!, authorizer: authorizer, factory: keysFactory, workQueue: workQueue, resultQueue: resultQueue)
+
+        let response = keysFetch.get("keys")
+        return response.map({
+            res in
+            if let r = res.successValue {
+                return Result(success: r.value)
+            }
+            return Result(failure: NSError(domain: "foo", code: 0, userInfo: nil))
+        })
+    }
+
     func testLive() {
         let expectation = expectationWithDescription("Waiting on value.")
 
+        // client: mgWl22CIzHiE
         let user = "holygoat+permatest@gmail.com"
         let state = withState(user, password: user)
 
         let token = state.bind {
             (stateResult: Result<FirefoxAccountState>) -> Deferred<Result<TokenServerToken>> in
             if let s = stateResult.successValue {
-                if let married = s as? FirefoxAccountState.Married {
-                    let audience = TokenServerClient.getAudienceForURL(ProductionSync15Configuration().tokenServerEndpointURL)
-                    let clientState = FxAClient10.computeClientState(married.kB)
-                    let client = TokenServerClient()
-                    return client.token(married.generateAssertionForAudience(audience), clientState: clientState)
-                } else {
-                    return Deferred(value: Result(failure: NSError(domain: "foo", code: 0, userInfo: nil)))
-                }
-            } else {
-                return Deferred(value: Result(failure: stateResult.failureValue!))
+                return self.getToken(s)
             }
+            return Deferred(value: Result(failure: stateResult.failureValue!))
         }
 
+        if let married = state.value.successValue as? FirefoxAccountState.Married {
+            let keys: Deferred<Result<Record<KeysPayload>>> = token.bind {
+                tokenResult in
+                if let token = tokenResult.successValue {
+                    return self.getKeys(married, token: token)
+                }
+                return Deferred(value: Result(failure: NSError(domain: "foo", code: 0, userInfo: nil)))
+            }
+            keys.upon({
+                res in
+                if let rec = res.successValue {
+                    XCTAssert(rec.id == "keys", "GUID is correct.")
+                    XCTAssert(rec.modified > 1000, "modified is sane.")
+                    println("Body: \(rec.payload)")
+                } else {
+                    XCTFail("No key record.")
+                }
+                expectation.fulfill()
+            })
+        } else {
+            XCTFail("Not in state Married.")
+            return
+        }
+        /*
         token.upon( {
             tokenResult in
             if let token = tokenResult.successValue {
                 let endpoint = token.api_endpoint
                 XCTAssertTrue(endpoint.rangeOfString("services.mozilla.com") != nil, "We got a Sync server.")
-                let prodURI = NSURL(string: endpoint + "/storage/clients/")
+                let cryptoURI = NSURL(string: endpoint + "/storage/crypto/")
                 let authorizer: Authorizer = {
                     (r: NSMutableURLRequest) -> NSMutableURLRequest in
                     let helper = HawkHelper(id: token.id, key: token.key.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
@@ -67,13 +124,14 @@ class LiveStorageClientTests : LiveAccountTest {
 
                 if let married = state.value.successValue as? FirefoxAccountState.Married {
                 let keyBundle: KeyBundle = KeyBundle.fromKB(married.kB)
-                let factory: (String) -> ClientPayload? = Keys(defaultBundle: keyBundle).factory("clients")
+                let keysFactory: (String) -> KeysPayload? = Keys(defaultBundle: keyBundle).factory("keys")
 
                 let workQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
                 let resultQueue = dispatch_get_main_queue()
-                let otherClient = Sync15StorageClient(serverURI: prodURI!, authorizer: authorizer, factory: factory, workQueue: workQueue, resultQueue: resultQueue)
+                let keysFetch = Sync15StorageClient(serverURI: cryptoURI!, authorizer: authorizer, factory: keysFactory, workQueue: workQueue, resultQueue: resultQueue)
 
-                let deferred = otherClient.get("mgWl22CIzHiE")
+                    // client: mgWl22CIzHiE
+                let deferred = keysFetch.get("keys")
                 deferred.upon({ result in
                     println("Here: \(result)")
                     expectation.fulfill()
@@ -86,8 +144,9 @@ class LiveStorageClientTests : LiveAccountTest {
                 XCTAssertNil(tokenResult.failureValue as? NSError)
                 expectation.fulfill()
             }
-        })
 
+        })
+*/
     // TODO: why does this take 20 seconds to time out, when the 'else' block fulfills?
     waitForExpectationsWithTimeout(20) { (error) in
     XCTAssertNil(error, "\(error)")
